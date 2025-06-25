bcbe552638803c090806b87826151c6b2fa80fa1
zcdev <zcdev@hotmail.com>
2025-06-25 11:31:01 -0700

    Initialize Vite folder and upload image assets

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id="root">
+        <h1>Clicky Owls</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
