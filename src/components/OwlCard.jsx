b3eaa88d79fa017b02796cc4dd1bee5a7c2e9c96
zcdev <zcdev@hotmail.com>
2025-06-25 17:24:56 -0700

    refactor: move assets folder to public directory

--- src/components/OwlCard.jsx
+++ src/components/OwlCard.jsx
@@ -1,7 +1,7 @@
 export default function OwlCard({ owl, onClick }) {
     return (
         <div className="owl-card" onClick={onClick}>
-            <img src={`./src/assets/${owl.image}`} alt={`Owl ${owl.id}`} />
+            <img src={`/assets/${owl.image}`} alt={`Owl ${owl.id}`} />
         </div>
     )
 }
